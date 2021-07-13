f541162e05883120744825886c3dc267aaa59f86
gonzzoh <gmeza0118@gmail.com>
2021-07-13 12:52:35 -0400

    fixed Check a List

--- apps.js
+++ apps.js
@@ -23,22 +23,38 @@
 ///////////////////////////////////////////////////////////////////////
 // CHECK A LIST // 
 
-// function checkForStudent(studentName, studentArray){
-//     const presentStudents = studentArray.find(element => element === studentName) 
-//     if (!presentStudents) {
-//         console.log('false')
-//     }
-// }
-//     console.log(checkForStudent('Sal', ['Arifa', 'Siara', 'John', 'Miguel']));
- 
-   
+// function checkForStudent(name, students){
+    //     const presentStudents = students.find(element => element === name) 
+    //     if (!presentStudents) {
+        //         console.log('false')
+        //     }
+        //     return 
+        // }
+        //     console.log(checkForStudent('Sal', ['Arifa', 'Siara', 'John', 'Miguel']));
+        
+        // PSUEDOCODE //
+        // STEP 1: Loop over the studens array 
+        // STEP 2: Compare the name against the current student
+        // STEP 3: if the name matches - return true
+        
+        function checkForStudent(name, students){
+            for(let i = 0; i < students.length; i++) {
+                if(students[i] === name){
+                    return true 
+                } 
+            }
+            return false
+        }
+            console.log(checkForStudent('Sal', ['Arifa', 'Siara', 'John', 'Miguel']));
+        
+
 ///////////////////////////////////////////////////////////////////////
 // SUM ARRAY //
 
-const array = [1, 2, 3, 4, 5, 6];
-let sum = 0;
-for (let i = 0; i < array.length; i++) {
-    sum += array[1];
-}
+// const array = [1, 2, 3, 4, 5, 6];
+// let sum = 0;
+// for (let i = 0; i < array.length; i++) {
+//     sum += array[1];
+// }
 
-console.log(sum);
+// console.log(sum);
