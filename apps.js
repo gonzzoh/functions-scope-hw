b15262afcbe1b51ae003ef830d9b2a67aef63302
gonzzoh <gmeza0118@gmail.com>
2021-07-12 22:07:37 -0400

    palindromes checked

---
+++ apps.js
@@ -0,0 +1,30 @@
+// The parameters are the names listed in 
+// the function's definition, while the 
+// arguments are the actual values in the
+// function.
+
+// return changes the value of the function
+// console.log makes the computer run and 
+// log the function.
+
+///////////////////////////////////////////////////////////////////////
+
+function checkPalindrome(string) {
+  const lowercaseString = string.toLowerCase();
+  const reversedString = lowercaseString.split('').reverse().join('');
+  console.log(reversedString)
+
+  // Compare the lowercase string w/ the reversed string
+  return lowercaseString === reversedString
+}
+
+console.log(checkPalindrome('raDaR'));
+
+const wordIsPalindrome = 
+
+function checkPalindrome(Radar){
+    if (wordIsPalindrome) {
+console.log()
+    } 
+}
+
